3b6a5f83c5b87a7ce04a49076740c72a85d962aa
Albert Yang <AlbertYang@pal-nat184-111-045.itap.purdue.edu>
2014-10-19 02:25:31 -0400

    for heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+
+def palindrome? (s)
+  return false if s.empty?
+  return ture if s.length == 1
+  return s.downcase.reverse.gsub(/\W/, "") == s.downcase.gsub(/\W/, "")
+end 
