ee17706b94eb4aded14f2463e983a5e5da8ad48c
Albert Yang <AlbertYang@pal-nat184-096-207.itap.purdue.edu>
2014-10-21 18:37:36 -0400

    Add pin/usr Association

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
     @pins = Pin.all
@@ -42,6 +44,12 @@ class PinsController < ApplicationController
     def set_pin
       @pin = Pin.find(params[:id])
     end
+
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+    end
+
     # never trust parameters from the scary internet, only allow the while list through
     def pin_params
       params.require(:pin).permit(:description)
