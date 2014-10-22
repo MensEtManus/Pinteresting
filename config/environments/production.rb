8f8f66c2228df8d97395dc587de988ef6159fa98
Albert Yang <AlbertYang@Alberts-MacBook-Air.local>
2014-10-21 23:29:21 -0400

    fixed bug in config_environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,9 +81,7 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-end
-
-# Allows user to upload images to Amazon S3
+ # Allows user to upload images to Amazon S3
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
@@ -92,3 +90,7 @@ end
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
 }
+end
+
+
+
